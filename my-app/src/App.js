21a2cad441734a8228f9a465dc1821f8329536db
송민섭 <ms.song@stickint.kr>
2022-12-13 19:04:03 +0900

    Modify useState, useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,39 @@
 import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>스틱인터랙티브</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world, 헬로 월드
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Songminseob/React"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          배워보자 리액트
-        </a>
-      </header>
-      <body>
-        <h1>스틱인터랙티브</h1>
-      </body>
-    </div>
-  );
+import {useEffect, useState} from 'react'
+
+const App = () => {
+    const [number, setNumber] = useState(0);
+
+    useEffect(() => {
+        if(number){
+            console.log('넘버가 바뀐 값');
+            console.log(number);
+        }
+
+        return () => {
+            console.log('넘버가 바뀌기 전');
+            console.log(number);
+        }
+    }, [number])
+
+    const Inc = () => {
+        setNumber(number + 1);
+    }
+
+    const Des = () => {
+        setNumber(number - 1);
+    }
+
+    return (
+      <div>
+          <h1>{number}</h1>
+          <button onClick={Inc}>+1</button>
+          <button onClick={Des}>-1</button>
+      </div>
+    );
+
 }
 
 export default App;
